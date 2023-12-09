553699748ecfd59c1d5d9d6d37cd80ec9e9eaa88
Owen Murphy <69216945+bigskii@users.noreply.github.com>
2023-12-09 01:15:38 -0500

    Add files via upload

---
+++ content.js
@@ -0,0 +1,24 @@
+const targetElement = document.querySelector('.biggish.redtext');
+
+if (document.URL.startsWith('https://www.tabroom.com/user/student/history.mhtml?tourn_id')) {
+    const currentURL = window.location.href;
+
+    const url = new URL(currentURL);
+
+    const tournId = url.searchParams.get("tourn_id");
+    console.log(tournId);
+    let targetURL = ["https://www.tabroom.com/index/tourn/index.mhtml?tourn_id=" + tournId];
+
+    if (targetElement) {
+        const anchorElement = document.createElement("a");
+
+        anchorElement.href = targetURL;
+        targetElement.textContent = "Tournament home";
+        while (targetElement.firstChild) {
+            anchorElement.appendChild(targetElement.firstChild);
+            targetElement.parentNode.replaceChild(anchorElement,targetElement);
+        }
+    } else {
+        console.error("Element not found")
+    }
+}
