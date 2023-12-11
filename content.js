41f0b665e2ebbd1908044f83b2dba7a813fdc74f
Owen Murphy <69216945+bigskii@users.noreply.github.com>
2023-12-10 22:38:34 -0500

    remove tew tab buttons

--- content.js
+++ content.js
@@ -1,24 +1,30 @@
 const targetElement = document.querySelector('.biggish.redtext');
 
-if (document.URL.startsWith('https://www.tabroom.com/user/student/history.mhtml?tourn_id')) {
-    const currentURL = window.location.href;
 
-    const url = new URL(currentURL);
+if (document.URL.startsWith('https://www.tabroom.com')) {
+    document.querySelectorAll('[target="_blank"]').forEach(element => {
+        element.removeAttribute("target");
+    });
+    if (document.URL.startsWith('https://www.tabroom.com/user/student/history.mhtml?tourn_id=')) {
+        const currentURL = window.location.href;
 
-    const tournId = url.searchParams.get("tourn_id");
-    console.log(tournId);
-    let targetURL = ["https://www.tabroom.com/index/tourn/index.mhtml?tourn_id=" + tournId];
+        const url = new URL(currentURL);
 
-    if (targetElement) {
-        const anchorElement = document.createElement("a");
+        const tournId = url.searchParams.get("tourn_id");
+        console.log(tournId);
+        let targetURL = ["https://www.tabroom.com/index/tourn/index.mhtml?tourn_id=" + tournId];
 
-        anchorElement.href = targetURL;
-        targetElement.textContent = "Tournament home";
-        while (targetElement.firstChild) {
-            anchorElement.appendChild(targetElement.firstChild);
-            targetElement.parentNode.replaceChild(anchorElement,targetElement);
+        if (targetElement) {
+            const anchorElement = document.createElement("a");
+
+            anchorElement.href = targetURL;
+            targetElement.textContent = "Tournament home";
+            while (targetElement.firstChild) {
+                anchorElement.appendChild(targetElement.firstChild);
+                targetElement.parentNode.replaceChild(anchorElement,targetElement);
+            }
+        } else {
+            console.error("Element not found")
         }
-    } else {
-        console.error("Element not found")
     }
 }
