f97474699a53b628a7da014617fdd9ac6c265d00
Owen <owen@owenmurphy.xyz>
2025-09-29 15:46:53 -0400

    fix command and add framework for judge page

--- background/background.js
+++ background/background.js
@@ -1,3 +1,18 @@
 chrome.runtime.onInstalled.addListener(() => {
     console.log("Installed");
 })
+
+function opentab() {
+    chrome.tabs.create({
+        url: "https://www.tabroom.com/",
+        selected: true,
+    })
+}
+
+function openshort(command) {
+    if (command === "opentab") {
+        opentab();
+    }
+}
+
+chrome.commands.onCommand.addListener(openshort);
