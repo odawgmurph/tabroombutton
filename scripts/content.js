fc6294eb6b09e08e361ae90ee70d9d41e8afaab9
verona <owen@owenmurphy.xyz>
2025-10-27 16:05:21 -0400

    change img host to imgur + name change!

--- scripts/content.js
+++ scripts/content.js
@@ -1,5 +1,5 @@
 // pages to title
-// -upcoming tourns
+// upcoming tourns
 // personal entries
 // results page
 // tourn home
@@ -29,17 +29,17 @@ if (overlay.style.backgroundImage != "url(/lib/images/lens-flair.png)") {
             if (result == undefined || result["flag"] == "gay") {
                 
             } else if (result["flag"] == "bi") {
-                overlay.style.backgroundImage = "url(https://lh3.googleusercontent.com/drive-storage/AJQWtBOcmQMnhRtE4rCav37gOUpTjjUmqlWXl5TZ9MZfdTBk7W4LGurQGBZXLor7rEpL37Ukc7bHuLTOcPLukBwKrH52Vb8YlpFxKPF8etYl6cCdyIc=w1901-h988)"
+                overlay.style.backgroundImage = "url(https://i.imgur.com/WpmYHAh.png)";
             } else if (result["flag"] == "trans") {
-                overlay.style.backgroundImage = "url(https://lh3.googleusercontent.com/drive-storage/AJQWtBN9EdkyLd0DfVoBgAvrji9X-l0B--0hXyBXGcBR4OJ8i8xC7ja2gwK3_yKDABsdmEIm2jaD1M4_1pnRSvkM-9euC3zlgow4it64SomszhKEdHE=w1901-h988)"
+                overlay.style.backgroundImage = "url(https://i.imgur.com/zPVHFnM.png)";
             } else if (result["flag"] == "les") {
-                overlay.style.backgroundImage = "url(https://lh3.googleusercontent.com/drive-storage/AJQWtBMpn2Cs93oPypezAu8C2c0sxULffyObQqqVUo6ERhwU0Q-iDmbH5LPUP6sYTcoDHIWVkRjiFr8ABZVVtTKcG2CoZiyLtHeF21BHUDSH42K27Q=w1901-h988)"
+                overlay.style.backgroundImage = "url(https://i.imgur.com/zUCfnua.png)";
             } else if (result["flag"] == "nb") {
-                overlay.style.backgroundImage = "url(https://lh3.googleusercontent.com/drive-storage/AJQWtBNNStaMSasvhuDmU1rwMIQBVJbfcCF8YrSn-yd9u9FSxVow64TJ4Bj3tqiV_LRqkCOCzc82CNGrNHI9X9Yrwi8Yz7mal28iVYcvrdFv2VX4kFY=w1901-h988)"
+                overlay.style.backgroundImage = "url(https://i.imgur.com/7MDyUuG.png)";
             } else if (result["flag"] == "ace") {
-                overlay.style.backgroundImage = "url(https://lh3.googleusercontent.com/drive-storage/AJQWtBOIBHnvldHbF239F6oiewfDGQd991Bt7cvQJWTq0Y6235A2KkXDh2nijSRhvIAu7M_PZjFwY5Hl61ovH_1KBeBRei_agkNrQNh5ax1a_6CZ2HA=w1143-h988)"
+                overlay.style.backgroundImage = "url(https://i.imgur.com/EVBGuns.png)";
             } else if (result["flag"] == "ger") {
-                overlay.style.backgroundImage = "url(https://lh3.googleusercontent.com/drive-storage/AJQWtBNwxFdn649YMrLegVmEi574mIVjxS6toM_FEwGq_VWc57zwzgC0WmV7TKNBp8N3D7nIoK-6ClQC-ggEOnm3SjUHrek9cJxEDa0SE939UCj8iX8=w1143-h988)"
+                overlay.style.backgroundImage = "url(https://i.imgur.com/GfdjQ1p.png)";
             }
         }))
 }
